6a4d0b6fe28a1c77707b562631ce2be9c7e65c7c
arthur33b <arthur72gb@gmail.com>
2025-11-24 13:15:01 -0300

    exercicio-organizador

---
+++ Organizador.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+import tkinter
+from tkinter import filedialog
+
+
+def escolher_pasta():
+    pasta = filedialog.askdirectory()
+    return pasta
+
+def criar_pastas(folder):
+    lista_pastas =["PDF","JSON","XLSX","IMG","Audio","Video","DOC","TXT"]
+    for nm_pasta in lista_pastas:
+        if not os.path.exists(os.path.join(folder,nm_pasta)):
+            os.makedirs(os.path.join(folder,nm_pasta))
+        else:
+            print(f"A pasta {nm_pasta}, já existe!")
+
+
+def main(folder):
+    os.chdir(folder)
+    lista_arquivos = os.listdir(folder)
+    txt = os.path.join(folder,"TXT")
+    pdf = os.path.join(folder,"PDF")
+    json = os.path.join(folder,"JSON")
+    xlsx = os.path.join(folder,"XLSX")
+    img = os.path.join(folder,"IMG")
+    audio = os.path.join(folder,"Audio")
+    video = os.path.join(folder,"Video")
+    doc = os.path.join(folder,"DOC")
+
+    for arquivo in lista_arquivos:
+        if os.path.isdir(os.path.join(folder,arquivo)):
+            continue
+        if arquivo.lower().endswith(".txt"):
+            print(arquivo)
+            shutil.move(os.path.join(folder, arquivo), os.path.join(txt, arquivo))
+        elif arquivo.lower().endswith(".pdf"):
+            print(arquivo)
+            shutil.move(os.path.join(folder, arquivo), os.path.join(pdf,arquivo))
+        elif arquivo.lower().endswith(".json"):
+            print(arquivo)
+            shutil.move(os.path.join(folder, arquivo), os.path.join(json,arquivo))
+        elif arquivo.lower().endswith(".xlsx") or arquivo.lower().endswith(".csv"):
+            print(arquivo)
+            shutil.move(os.path.join(folder, arquivo), os.path.join(xlsx,arquivo))
+
